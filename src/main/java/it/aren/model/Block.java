7a136198685525785da88d08fefe2fcf88a45193
Lorenzo Signoretti <lory4846@hotmail.it>
2022-03-22 18:23:01 +0100

    Create Block.java

---
+++ src/main/java/it/aren/model/Block.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package it.aren.model;
+
+import it.aren.common.Point2D;
+
+/**
+ * 
+ */
+public class Block extends Entity {
+
+    public Block(final Point2D position, final boolean drawable) {
+        super(position, drawable);
+        // TODO when events are implemented
+    }
+
+    @Override
+    public void updateGraphic() {
+        // TODO when graphic is implemented
+
+    }
+
+}
