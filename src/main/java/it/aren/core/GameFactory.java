03962e4e5ebb7ae320fc4e239a28f0af20041326
Alessia <alessia.lombardi4@studio.unibo.it>
2022-03-30 11:19:38 +0200

    Add GameFactory

---
+++ src/main/java/it/aren/core/GameFactory.java
@@ -0,0 +1,22 @@
+package it.aren.core;
+
+import it.aren.common.Point2D;
+import it.aren.model.*;
+
+public class GameFactory {
+
+    private static GameFactory instance;
+
+    public static GameFactory getInstance() {
+        if (instance == null) {
+            instance = new GameFactory();
+        }
+        return instance;
+    }
+    public final Player createPlayer(final Point2D position) {
+        return new Player(position);
+    }
+    public final Block createBlock(final Point2D position, final boolean drawable) {
+        return new Block(position, drawable);
+    }
+}
