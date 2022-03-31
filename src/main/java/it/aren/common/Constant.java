bbe0a914db552cda54af636c1da19dc46d477715
Lorenzo Signoretti <lory4846@hotmail.it>
2022-03-31 23:18:22 +0200

    Constant for directory

--- src/main/java/it/aren/common/Constant.java
+++ src/main/java/it/aren/common/Constant.java
@@ -11,4 +11,7 @@ public class Constant {
     public final static int DEFAULT_MAP_DIMENSION = 16;
     public final static int DEFAULT_HITBOX_DIMENSION = 32;
     public static final long DEFAULT_FPS = 33; // =~ 30 frames per second
+    public final static String HOME = System.getProperty("user.home");
+    public final static String SEP = System.getProperty("file.separator");
+    public final static String MAIN_FOLDER = HOME +  SEP + ".aren";
 }
