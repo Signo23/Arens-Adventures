b55792350a014459cbae92f2d8810c8128042aef
Lorenzo Signoretti <lory4846@hotmail.it>
2022-03-31 20:40:02 +0200

    Create GameEngine.java

---
+++ src/main/java/it/aren/core/GameEngine.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package it.aren.core;
+
+import it.aren.common.Constant;
+import it.aren.graphic.SwingView;
+import it.aren.graphic.View;
+
+/**
+ * 
+ *
+ */
+public class GameEngine {
+    private View view;
+
+    void setup() {
+        this.view = new SwingView();
+    }
+
+    void loop() {
+        final long current = System.currentTimeMillis();
+        this.processInput();
+        this.updateGame();
+        this.render();
+        this.waitNextFrame(current);
+    }
+
+    private void processInput() {
+        // TODO
+    }
+
+    private void updateGame() {
+        // TODO updates events
+    }
+
+    private void render() {
+        this.view.render();
+    }
+
+    private void waitNextFrame(final long current) {
+        final long dt = System.currentTimeMillis() - current;
+        if (dt < Constant.DEFAULT_FPS) {
+            try {
+                Thread.sleep(Constant.DEFAULT_FPS - dt);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
