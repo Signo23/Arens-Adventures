295ef330707be8514a51710505d83b8463f618e3
Giulia Montini <giulia.montini2@studio.unibo.it>
2022-04-04 20:31:46 +0200

    Update Constant.java

--- src/main/java/it/aren/common/Constant.java
+++ src/main/java/it/aren/common/Constant.java
@@ -15,7 +15,8 @@ public class Constant {
     public final static String SEP = System.getProperty("file.separator");
     public final static String MAIN_FOLDER = HOME +  SEP + ".aren";
     public final static Point2D LEFT = new Point2D(-1, 0);
-    public final static Point2D RIGHT = new Point2D(0, -1);
+    public final static Point2D RIGHT = new Point2D(1, 0);
     public final static Point2D UP = new Point2D(0, 1);
     public final static Point2D DOWN = new Point2D(0, -1);
+    public final static int DEFAULT_VEL = 10;
 }
