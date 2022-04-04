6d2f1869e21704e8d3c34f74db07974edf4f9385
Alessia <alessia.lombardi4@studio.unibo.it>
2022-04-04 19:46:14 +0200

    Add idle control

--- src/main/java/it/aren/input/PlayerInputComponent.java
+++ src/main/java/it/aren/input/PlayerInputComponent.java
@@ -10,15 +10,22 @@ public class PlayerInputComponent implements InputComponent {
         if (controller.isMoveUp()) {
             //Aren si muove verso l'alto
             ((Player) entity).setLastDirection(new Point2D(0, 1));
+            ((Player) entity).setIdle(false);
         } else if (controller.isMoveDown()) {
             //Aren si muove verso il basso
             ((Player) entity).setLastDirection(new Point2D(0, -1));
+            ((Player) entity).setIdle(false);
         } else if (controller.isMoveLeft()) {
             //Aren si muove verso snistra
             ((Player) entity).setLastDirection(new Point2D(-1, 0));
+            ((Player) entity).setIdle(false);
         } else if (controller.isMoveRight()) {
             //Aren si muove verso destra
             ((Player) entity).setLastDirection(new Point2D(1, 0));
+            ((Player) entity).setIdle(false);
+        } else {
+            //Aren sta oziando
+            ((Player) entity).setIdle(true);
         }
     }
 
