337de14271aea3306ecb0165dc2ddd69aa5a49ab
Alessia <alessia.lombardi4@studio.unibo.it>
2022-04-06 10:25:16 +0200

    Fix direction coordinates

--- src/main/java/it/aren/input/PlayerInputComponent.java
+++ src/main/java/it/aren/input/PlayerInputComponent.java
@@ -1,27 +1,27 @@
 package it.aren.input;
 
-import it.aren.common.Point2D;
 import it.aren.model.Entity;
 import it.aren.model.Player;
+import it.aren.common.Constant;
 
 public class PlayerInputComponent implements InputComponent {
 
     public final void update(final Entity entity, final InputController controller) {
         if (controller.isMoveUp()) {
             //Aren si muove verso l'alto
-            ((Player) entity).setLastDirection(new Point2D(0, 1));
+            ((Player) entity).setLastDirection(Constant.UP);
             ((Player) entity).setIdle(false);
         } else if (controller.isMoveDown()) {
             //Aren si muove verso il basso
-            ((Player) entity).setLastDirection(new Point2D(0, -1));
+            ((Player) entity).setLastDirection(Constant.DOWN);
             ((Player) entity).setIdle(false);
         } else if (controller.isMoveLeft()) {
             //Aren si muove verso snistra
-            ((Player) entity).setLastDirection(new Point2D(-1, 0));
+            ((Player) entity).setLastDirection(Constant.LEFT);
             ((Player) entity).setIdle(false);
         } else if (controller.isMoveRight()) {
             //Aren si muove verso destra
-            ((Player) entity).setLastDirection(new Point2D(1, 0));
+            ((Player) entity).setLastDirection(Constant.RIGHT);
             ((Player) entity).setIdle(false);
         } else {
             //Aren sta oziando
