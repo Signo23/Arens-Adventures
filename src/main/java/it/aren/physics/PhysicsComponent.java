151d59ec173e1b8e270cc0a20ac3c35547b00603
Giulia Montini <giulia.montini2@studio.unibo.it>
2022-04-07 10:21:46 +0200

    Update PhysicsComponent.java

--- src/main/java/it/aren/physics/PhysicsComponent.java
+++ src/main/java/it/aren/physics/PhysicsComponent.java
@@ -3,10 +3,12 @@
  */
 package it.aren.physics;
 
+import it.aren.model.Entity;
+
 /**
  *
  *
  */
 public interface PhysicsComponent {
-  void update(Entity entity);
+    void update(Entity entity);
 }
