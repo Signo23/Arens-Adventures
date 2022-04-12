c949c60d5308de6549a9058a7dc56ce08a7adc53
Lorenzo Signoretti <lory4846@hotmail.it>
2022-04-12 10:28:36 +0200

    Costanti bilanciate per velocità e FPS

--- src/main/java/it/aren/common/Constant.java
+++ src/main/java/it/aren/common/Constant.java
@@ -10,7 +10,7 @@ package it.aren.common;
 public class Constant {
     public final static int DEFAULT_MAP_DIMENSION = 16;
     public final static int DEFAULT_HITBOX_DIMENSION = 32;
-    public static final long DEFAULT_FPS = 33; // =~ 30 frames per second
+    public static final long DEFAULT_FPS = 16; // 33 =~ 30 frames per second
     public final static String HOME = System.getProperty("user.home");
     public final static String SEP = System.getProperty("file.separator");
     public final static String MAIN_FOLDER = HOME +  SEP + ".aren";
@@ -18,5 +18,5 @@ public class Constant {
     public final static Point2D RIGHT = new Point2D(1, 0);
     public final static Point2D UP = new Point2D(0, -1);
     public final static Point2D DOWN = new Point2D(0, 1);
-    public final static int DEFAULT_VEL = 10;
+    public final static int DEFAULT_VEL = 2;
 }
