78246a3d57b7e000ddd2917ba9c9ecf620c511b6
Alessia <alessia.lombardi4@studio.unibo.it>
2022-04-13 20:23:34 +0200

    Add JavaDoc

--- src/main/java/it/aren/core/GameFactory.java
+++ src/main/java/it/aren/core/GameFactory.java
@@ -1,3 +1,6 @@
+/**
+ * 
+ */
 package it.aren.core;
 
 import it.aren.common.Point2D;
@@ -6,18 +9,31 @@ import it.aren.model.GameMap;
 import it.aren.model.Player;
 import it.aren.physics.PlayerPhysicsComponent;
 import it.aren.graphic.PlayerGraphicComponent;
-
+/**
+ * The main factory.
+ */
 public final class GameFactory {
 
+    /**
+     * The GameFactory constructor.
+     */
     private GameFactory() {
 
     }
+    /**
+     * Creates a new player.
+     * @return player
+     */
     public static Player createPlayer() {
         return new Player(new Point2D(0, 1),
                 new PlayerGraphicComponent(),
                 new PlayerInputComponent(),
                 new PlayerPhysicsComponent());
     }
+    /**
+     * Loads the game maps.
+     * @return GameMap
+     */
     public static GameMap loadMaps() {
         return new GameMap();
     }
