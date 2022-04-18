be7242c73043a6417b60bc0865e572ef0368089d
Lorenzo Signoretti <lory4846@hotmail.it>
2022-04-18 21:16:28 +0200

    Add commentes for generate JavaDoc

--- src/main/java/it/aren/event/Event.java
+++ src/main/java/it/aren/event/Event.java
@@ -3,10 +3,15 @@
  */
 package it.aren.event;
 
+import it.aren.model.World;
+
 /**
- * 
- *
+ * Interface for create events that interact with {@link World}.
  */
 public interface Event {
-    void launch();
+    /**
+     * Launch the event.
+     * @param world {@link World} to interact with
+     */
+    void launch(World world);
 }
