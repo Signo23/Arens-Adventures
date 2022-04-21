f7c453534b989c841a7d54c3cb838f7ced402ea3
Lorenzo Signoretti <lory4846@hotmail.it>
2022-04-21 19:15:31 +0200

    Create Settings for manage game's settings

---
+++ src/main/java/it/aren/common/Settings.java
@@ -0,0 +1,68 @@
+package it.aren.common;
+
+import java.awt.Dimension;
+/**
+ * Manage settings of the game.
+ */
+public class Settings {
+    private boolean antialiasingOn;
+    private Dimension screenSize;
+    
+    /**
+     * Load default settings from {@link Constant}
+     */
+    public Settings() {
+        this.antialiasingOn = false;
+        this.screenSize = Constant.DEFAULT_SCREEN_DIMENSION;
+    }
+
+    /**
+     * Calculate the dimension of hitbox.
+     * @return hitbox's {@link Dimension}
+     */
+    public Dimension hitboxDimension() {
+        final int hitbox = this.screenSize.height / Constant.DEAFULT_MAP_HEIGHT;
+        return new Dimension(hitbox, hitbox);
+    }
+    
+    /**
+     * Calculate the scale for correct sprite load.
+     * @return the correct scale for sprite
+     */
+    public int spriteScale() {
+        return this.hitboxDimension().height / Constant.DEFAULT_HITBOX_DIMENSION;
+    }
+    
+    /**
+     * Get the status for antialiasing's setting
+     * @return the antialiasingOn
+     */
+    public boolean isAntialiasingOn() {
+        return antialiasingOn;
+    }
+
+    /**
+     * Set the antialiasing.
+     * @param antialiasingOn the antialiasingOn to set
+     */
+    public void setAntialiasingOn(final boolean antialiasingOn) {
+        this.antialiasingOn = antialiasingOn;
+    }
+
+    /**
+     * Get the screen {@link Dimension}
+     * @return the screenSize
+     */
+    public Dimension getScreenSize() {
+        return screenSize;
+    }
+
+    /**
+     * Set the screen {@link Dimension}
+     * @param screenSize the screenSize to set
+     */
+    public void setScreenSize(final Dimension screenSize) {
+        this.screenSize = screenSize;
+    }
+    
+}
