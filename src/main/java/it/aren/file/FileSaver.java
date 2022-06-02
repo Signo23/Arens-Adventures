faaad6bd799127cfb8b57aae038031a06c3da226
Giulia Montini <giulia.montini2@studio.unibo.it>
2022-06-02 17:47:04 +0200

    Create FileSaver interface

---
+++ src/main/java/it/aren/file/FileSaver.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package it.aren.file;
+
+/**
+ * The file saver interface to write file.
+ * @param <T> is the parameter type for writing files
+ */
+public interface FileSaver<T> {
+    
+    /**
+     * Method to write a file.
+     * @param objToSave object to save
+     * @param fileName file’s name to write
+     */
+    void saveFile(T objToSave, String fileName);
+}
