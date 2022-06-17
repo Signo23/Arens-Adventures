365d8d4c1bae09f53b58f3e1ceb08ed6cecfca3e
Giulia Montini <giulia.montini2@studio.unibo.it>
2022-06-17 17:34:25 +0200

    Update MenuPanel.java

--- src/main/java/it/aren/graphic/menu/MenuPanel.java
+++ src/main/java/it/aren/graphic/menu/MenuPanel.java
@@ -13,6 +13,7 @@ import it.aren.common.BaseObjectEnum;
 import it.aren.common.Constant;
 import it.aren.common.Settings;
 import it.aren.file.SettingsLoader;
+import it.aren.input.MenuInputController;
 
 /**
  *
@@ -21,7 +22,7 @@ import it.aren.file.SettingsLoader;
 public class MenuPanel extends JPanel {
 
     private static final long serialVersionUID = 1L;
-    public MenuPanel() {
+    public MenuPanel(final MenuInputController controller) {
         final Settings settings = SettingsLoader.loadSettings();
         final int dimension = Constant.DEFAULT_HITBOX_DIMENSION * settings.scale();
         final MenuButton playButton = new MenuButton(BaseActionEnum.PLAY.getTexture(), "Gioca");
@@ -31,6 +32,13 @@ public class MenuPanel extends JPanel {
 
         playButton.setBounds(4 * dimension, 6 * dimension, 8 * dimension, 1 * dimension);
         settingsButton.setBounds(4 * dimension, (int)(7.5 * dimension), 8 * dimension, 1 * dimension);
+
+        playButton.addActionListener(a -> {
+            controller.notifyIsInteract();
+        });
+        settingsButton.addActionListener(a -> {
+            controller.notifyIsSettings();
+        });
 
         this.add(playButton);
         this.add(settingsButton);
