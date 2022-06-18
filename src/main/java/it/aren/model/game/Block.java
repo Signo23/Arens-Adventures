d1699638c32c49c33ef920568f7d220c15a80781
Lorenzo Signoretti <lory4846@hotmail.it>
2022-06-19 00:56:21 +0200

    Remove deprecated constructors in Block

--- src/main/java/it/aren/model/game/Block.java
+++ src/main/java/it/aren/model/game/Block.java
@@ -4,10 +4,8 @@ import it.aren.common.Constant;
 import it.aren.common.BaseObjectEnum;
 import it.aren.common.Point2D;
 import it.aren.event.Event;
-import it.aren.event.NullEvent;
 import it.aren.file.SettingsLoader;
 import it.aren.graphic.GraphicComponent;
-import it.aren.model.BaseEntity;
 
 /**
  * Model for every block.
@@ -16,7 +14,6 @@ import it.aren.model.BaseEntity;
 public class Block extends GameObject {
 
     private Event event;
-    
     /**
      * Create a Block.
      * @param event the {@link Event} to set in the block
@@ -30,45 +27,6 @@ public class Block extends GameObject {
         final int ratio = SettingsLoader.loadSettings().scale();
         this.event = event;
         this.hitBox.setSize(ratio * Constant.DEFAULT_HITBOX_DIMENSION, ratio * Constant.DEFAULT_HITBOX_DIMENSION / 2);
-    }
-
-    @Deprecated
-    /**
-     * @deprecated
-     * Create a Block.
-     * @param position where block is
-     * @param drawable set if the block will be drawn
-     * @param graphic for draw the block
-     */
-    public Block(final Point2D position, final boolean drawable, final GraphicComponent graphic) {
-        this(new NullEvent() ,position, drawable, graphic, Constant.DEFAULT_RATIO);
-    }
-    
-    @Deprecated
-    /**
-     * @deprecated
-     * Create a Block.
-     * @param event the {@link Event} to set in the block
-     * @param position where block is
-     * @param drawable set if the block will be drawn
-     * @param graphic for draw the block
-     * @param ratio ratio for hiBox
-     */
-    public Block(final Event event, final Point2D position, final boolean drawable, final GraphicComponent graphic, final int ratio) {
-        this(BaseObjectEnum.VOID, event, position, drawable, graphic);
-    }
-    
-    @Deprecated
-    /**
-     * @deprecated
-     * Create a Block.
-     * @param event the {@link Event} to set in the block
-     * @param position where block is
-     * @param drawable set if the block will be drawn
-     * @param graphic for draw the block
-     */
-    public Block(final Event event, final Point2D position, final boolean drawable, final GraphicComponent graphic){
-        this(event, position, drawable, graphic, Constant.DEFAULT_RATIO);
     }
 
     /**
