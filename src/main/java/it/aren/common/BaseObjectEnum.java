1b27ea9de241867c790e1ab5cc785f34668894d4
Lorenzo Signoretti <lory4846@hotmail.it>
2022-06-18 10:22:22 +0200

    Update BaseObjectEnum's JavaDoc

--- src/main/java/it/aren/common/BaseObjectEnum.java
+++ src/main/java/it/aren/common/BaseObjectEnum.java
@@ -5,20 +5,63 @@ import java.awt.image.BufferedImage;
 import it.aren.file.ImageLoader;
 import it.aren.file.SettingsLoader;
 
+/**
+ * Type that could be used for every type of Object.
+ *
+ */
 public enum BaseObjectEnum {
 
+    /**
+     * Chest type.
+     */
     CHEST("chest.png"),
+    /**
+     * Sign type.
+     */
     SIGN("sign.png"),
+    /**
+     * NPC type.
+     */
     NPC("npc.png"),
+    /**
+     * Box type.
+     */
     BOX("box.png"),
+    /**
+     * Potion type.
+     */
     POTION("potion.png"),
+    /**
+     * Jacket type.
+     */
     JACKET("jacket.png"),
+    /**
+     * Key type.
+     */
     KEY("key.png"),
+    /**
+     * Sword type.
+     */
     SWORD("sword.png"),
+    /**
+     * Void type.
+     */
     VOID(),
+    /**
+     * Player idle type.
+     */
     PLAYER_IDLE("player-idle.png"),
+    /**
+     * Player walk type.
+     */
     PLAYER_WALK("player-walk.png"),
+    /**
+     * Background type.
+     */
     BACKGROUND("banner.png"),
+    /**
+     * Icon type.
+     */
     ICON("icon.png");
 
     private final BufferedImage texture;
