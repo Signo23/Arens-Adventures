5addc1810558222e739557e1e976563eb83a664d
Lorenzo Signoretti <lory4846@hotmail.it>
2022-09-21 23:00:25 +0200

    Create a custom hit box

---
+++ src/main/java/it/aren/model/HitBox.java
@@ -0,0 +1,28 @@
+package it.aren.model;
+
+import it.aren.common.Point2D;
+
+public interface HitBox {
+    /**
+     * Determines whether or not this {@code HitBox} and the specified
+     * {@code HitBox} collide. Two hitboxes collide if
+     * their intersection is nonempty.
+     * @param hb the specified {@code HitBox}
+     * @return {@code true} if the specified {@code HitBox}
+     *      and this {@code HitBox} collide;
+     *      {@code false} otherwise.
+     */
+    boolean isColliding(HitBox hb);
+
+    /**
+     * Determinate the position of the hit box.
+     * @return a {@link Point2D} containg the position
+     */
+    Point2D position();
+
+    /**
+     * Determinate the dimension of the hit box.
+     * @return a {@link Point2D} containing the dimension
+     */
+    Point2D dimension();
+}
---
+++ src/main/java/it/aren/model/game/GameHitBox.java
@@ -0,0 +1,62 @@
+package it.aren.model.game;
+
+import it.aren.common.Point2D;
+import it.aren.model.HitBox;
+
+public class GameHitBox implements HitBox {
+
+    private Point2D position;
+    private Point2D dimension;
+
+    public GameHitBox (final Point2D position, final Point2D dimension){
+        this.dimension = dimension;
+        this.position = position;
+    }
+
+    public GameHitBox(final float x, final float y, final float width, final float height){
+        this(new Point2D(x, y), new Point2D(width, height));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean isColliding(HitBox hb) {
+        float thisWidth = this.dimension.getX();
+        float thisHeight = this.dimension.getY();
+        float rw = hb.dimension().getX();
+        float rh = hb.dimension().getY();
+        if (rw <= 0 || rh <= 0 || thisWidth <= 0 || thisHeight <= 0) {
+            return false;
+        }
+        float tx = this.position.getX();
+        float ty = this.position.getY();
+        float rx = hb.position().getX();
+        float ry = hb.position().getY();
+        rw += rx;
+        rh += ry;
+        thisWidth += tx;
+        thisHeight += ty;
+        //      overflow || intersect
+        return ((rw < rx || rw > tx) &&
+                (rh < ry || rh > ty) &&
+                (thisWidth < tx || thisWidth > rx) &&
+                (thisHeight < ty || thisHeight > ry));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Point2D position() {
+        return this.position;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Point2D dimension() {
+        return this.dimension;
+    }
+}
